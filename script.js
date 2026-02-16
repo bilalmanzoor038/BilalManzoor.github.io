e379cc5bc2d104a719dee50135b1b7c9a9d9a78d
Bilal Manzoor <142536165+bilalmanzoor038@users.noreply.github.com>
2026-02-16 16:17:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function(e) {
         e.preventDefault();
+
         document.querySelector(this.getAttribute("href")).scrollIntoView({
             behavior: "smooth"
         });
     });
 });
-
-// Scroll reveal animation
-function reveal() {
-    let reveals = document.querySelectorAll(".reveal");
-
-    reveals.forEach(element => {
-        let windowHeight = window.innerHeight;
-        let elementTop = element.getBoundingClientRect().top;
-        let revealPoint = 100;
-
-        if (elementTop < windowHeight - revealPoint) {
-            element.classList.add("active");
-        }
-    });
-}
-
-window.addEventListener("scroll", reveal);
-window.addEventListener("load", reveal);
